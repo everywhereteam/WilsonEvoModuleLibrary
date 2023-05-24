f38667fd94b65c350c9ef5fa1f5e8c0d7844de62
TheSmallPixel <thesmallpixel@gmail.com>
2023-05-24 12:37:39 +0200

    ypyp

---
+++ Attributes/ServiceNodeDefinition.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WilsonEvoModuleLibrary.Attributes
+{
+    public class ServiceNodeDefinition
+    {
+        public List<ServiceNodePropertyDefinition> PropertyDefinitions { get; set; } = new();
+
+
+    }
+
+    public class ServiceNodePropertyDefinition
+    {
+        public string Name { get; set; }
+        public string? DisplayName { get; set; }
+        public DataType? DataType { get; set; }
+        public string? CustomDataType { get; set; }
+
+        public string[] SelectionStrings { get; set; }
+    }
+
+    public static class ServiceNodePropertyDefinitionExtensions
+    {
+        public static ServiceNodeDefinition GetDefinition<T>(this T obj)
+        {
+            var definition = new ServiceNodeDefinition();
+            var proList = typeof(T).GetProperties();
+            foreach (var prp in proList)
+            {
+                var attrList = (DataTypeAttribute)prp.GetCustomAttributes(typeof(DataTypeAttribute), false).First();
+                var displayLabel = (DisplayAttribute)prp.GetCustomAttributes(typeof(DisplayAttribute), false).First();
+                // Get the initial property value
+                var servicePropDef = new ServiceNodePropertyDefinition()
+                {
+                    Name = prp.Name,
+                    DisplayName = displayLabel?.Name,
+                    DataType = attrList?.DataType,
+                    CustomDataType = attrList?.CustomDataType
+                };
+              
+                definition.PropertyDefinitions.Add(servicePropDef);
+
+            }
+            return definition;
+        }
+    }
+
+    
+}
