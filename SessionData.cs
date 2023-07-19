d15eb357852e32fca491e936dd4b366cebad5d21
TheSmallPixel <thesmallpixel@gmail.com>
2023-07-19 14:47:35 +0200

    35

--- SessionData.cs
+++ SessionData.cs
@@ -9,7 +9,7 @@ namespace WilsonEvoModuleLibrary
         {
         }
 
-        public SessionData(string sessionId, int processCode, JObject request = null)
+        public SessionData(string sessionId, int processCode, dynamic request = null)
         {
             Request = request;
             SessionId = sessionId;
@@ -27,7 +27,7 @@ namespace WilsonEvoModuleLibrary
         public string ChannelType { get; set; }
         public string Output { get; set; }
         public dynamic Request { get; set; }
-        public JObject Response { get; set; }
+        public dynamic Response { get; set; }
         public string CurrentNodeId { get; set; }
         public string CurrentShortUrl { get; set; }
         public string CurrentOutput { get; set; }
