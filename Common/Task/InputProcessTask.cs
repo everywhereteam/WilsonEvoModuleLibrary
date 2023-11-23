ea9cf9d79e16ffdb4d872a78db3d3d18841db09a
Edoardo Barchiesi <edoardo.barchiesi@hotmail.it>
2023-11-23 12:07:21 +0100

    Update InputProcessTask.cs
    Edited InputTask fields

--- Common/Task/InputProcessTask.cs
+++ Common/Task/InputProcessTask.cs
@@ -8,10 +8,8 @@ namespace WilsonEvoModuleLibrary.Common.Task;
 [Task("Input", Categories.IO, Output.Ok, Output.Error, Output.Timeout)]
 public class InputTask : BaseTask
 {
-    [Display(Name = "Message Confirmation"), DataType(DataType.Text)]
-    public string MessageConfirmation { get; set; }
-    [Display(Name = "Message Disambiguation"), DataType(DataType.Text)]
-    public string MessageDisambiguation { get; set; }
+    [Display(Name = "Message"), DataType(DataType.Text)]
+    public string Message { get; set; }
     [Display(Name = "Variable"), DataType(DataType.Text)]
     public string Variable { get; set; }
 }
