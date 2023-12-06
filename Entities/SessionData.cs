8054cd70cd8c2c0184e4686aaf3537c47804b42d
TheSmallPixel <thesmallpixel@gmail.com>
2023-12-06 12:08:39 +0100

    uppi uppi

--- Entities/SessionData.cs
+++ Entities/SessionData.cs
@@ -12,23 +12,41 @@ public class SessionData
     public string To { get; set; }
     public bool Test { get; set; }
     public string ChannelType { get; set; }
-    public Dictionary<string, byte[]?> ServiceSecrets { get; set; } = new();
     public string Output { get; set; }
+    /// <summary>
+    /// This is the data incoming from the channel controller.
+    /// </summary>
     public byte[]? Request { get; set; }
+    /// <summary>
+    /// This is for the data going out from the channel controller.
+    /// </summary>
     public byte[]? Response { get; set; }
     public string CurrentNodeId { get; set; }
     public string CurrentShortUrl { get; set; }
     public string CurrentOutput { get; set; }
-    public string CurrentPoolId { get; set; }
+    public string? CurrentPoolId { get; set; }
     public bool WaitingCallback { get; set; } = false;
     public bool ContinueExecution { get; set; } = true;
     public bool IsFaulted { get; set; } = false;
     public bool IsEndedCorrectly { get; set; } = false;
+    /// <summary>
+    /// This is true when the session has been already ended, and will not go forward.
+    /// </summary>
     public bool ItWasAlreadyEnded { get; set; } = false;
     public string Exception { get; set; } = string.Empty;
     public Dictionary<string, object> VarData { get; set; } = new();
+    /// <summary>
+    /// This is a special structure to verify that the session is not looping.
+    /// </summary>
     public Dictionary<string, int> OperationTracker { get; set; } = new();
+    /// <summary>
+    /// This contains the special information for the channel in use.
+    /// </summary>
     public Dictionary<string, string> ChannelData { get; set; } = new();
+    /// <summary>
+    /// This contains the secrets for the service, it's personal for modules.
+    /// </summary>
+    public Dictionary<string, byte[]?> ServiceSecrets { get; set; } = new();
 
 
 }
---
+++ Utility/StringTemplate.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WilsonEvoModuleLibrary.Utility
+{
+    public static class StringTemplate
+    {
+        public static string Interpolate(string template, Dictionary<string, object> variables)
+        {
+            JObject jObject = JObject.FromObject(new Dictionary<string, object>(variables));
+
+            return Regex.Replace(template, @"\{[\w\.]+\}", match =>
+            {
+                string path = match.Value.Trim('{', '}');
+                JToken token = jObject.SelectToken(path);
+                return token?.ToString() ?? string.Empty;
+            });
+        }
+    }
+}
