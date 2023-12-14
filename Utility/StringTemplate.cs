2745d944ae39508c8d59c4f900133b927ced8cc7
TheSmallPixel <thesmallpixel@gmail.com>
2023-12-14 18:03:41 +0100

    some changes

--- Utility/StringTemplate.cs
+++ Utility/StringTemplate.cs
@@ -1,25 +1,20 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
-namespace WilsonEvoModuleLibrary.Utility
+namespace WilsonEvoModuleLibrary.Utility;
+
+public static class StringTemplate
 {
-    public static class StringTemplate
+    public static string Interpolate(string template, Dictionary<string, object> variables)
     {
-        public static string Interpolate(string template, Dictionary<string, object> variables)
-        {
-            JObject jObject = JObject.FromObject(new Dictionary<string, object>(variables));
+        var jObject = JObject.FromObject(new Dictionary<string, object>(variables));
 
-            return Regex.Replace(template, @"\{[\w\.]+\}", match =>
-            {
-                string path = match.Value.Trim('{', '}');
-                JToken token = jObject.SelectToken(path);
-                return token?.ToString() ?? string.Empty;
-            });
-        }
+        return Regex.Replace(template, @"\{[\w\.]+\}", match =>
+        {
+            var path = match.Value.Trim('{', '}');
+            var token = jObject.SelectToken(path);
+            return token?.ToString() ?? string.Empty;
+        });
     }
 }
